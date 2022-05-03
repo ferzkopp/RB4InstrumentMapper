11fd34398b07b0ca952f12a1c1cfbc810aec2938
Nathan <nathanhurst03@gmail.com>
2022-05-03 17:17:36 -0600

    Fix ScaleToInt32 and 16

--- PacketParsing/ParsingUtils.cs
+++ PacketParsing/ParsingUtils.cs
@@ -3,18 +3,18 @@ using System;
 namespace RB4InstrumentMapper.Parsing
 {
     /// <summary>
-    /// Common helper functions used in parsing.
+    /// Helper functions for parsing.
     /// </summary>
     static class ParsingUtils
     {
         /// <summary>
-        /// Scales this byte to an int.
+        /// Scales this byte to an int, starting from the negative end.
         /// </summary>
         public static int ScaleToInt32(this byte input)
         {
             // Duplicate the input value to the higher 8-bit regions by multiplying by a number with the
-            // first bit of each region set to 1, then OR with the negative bit to ensure correct sign
-            return (int)((input * 0x01010101) | 0x80000000);
+            // first bit of each region set to 1, then XOR with the negative bit to make the range start from the negative end
+            return (int)((input * 0x01010101) ^ 0x80000000);
         }
 
         /// <summary>
@@ -28,13 +28,13 @@ namespace RB4InstrumentMapper.Parsing
         }
 
         /// <summary>
-        /// Scales a byte to a short.
+        /// Scales a byte to a short, starting from the negative end.
         /// </summary>
         public static short ScaleToInt16(this byte input)
         {
             // Duplicate the input value to the higher 8-bit regions by multiplying by a number with the
-            // first bit of each region set to 1, then OR with the negative bit to ensure correct sign
-            return (short)((input * 0x0101) | 0x8000);
+            // first bit of each region set to 1, then XOR with the negative bit to make the range start from the negative end
+            return (short)((input * 0x0101) ^ 0x8000);
         }
 
         /// <summary>
