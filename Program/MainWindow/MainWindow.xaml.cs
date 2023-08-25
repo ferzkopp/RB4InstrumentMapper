1cef73f2204809364d465229773f72eb2012d8a7
Nathan <nathanhurst03@gmail.com>
2023-08-25 04:00:43 -0600

    Move unhandled exception event handler to App

--- Program/MainWindow/MainWindow.xaml.cs
+++ Program/MainWindow/MainWindow.xaml.cs
@@ -59,9 +59,6 @@ namespace RB4InstrumentMapper
 
         public MainWindow()
         {
-            // Assign event handler for unhandled exceptions
-            AppDomain.CurrentDomain.UnhandledException += OnUnhandledException;
-
             InitializeComponent();
 
             var version = Assembly.GetEntryAssembly().GetName().Version;
@@ -711,62 +708,6 @@ namespace RB4InstrumentMapper
 
             // Refresh the dropdown
             PopulatePcapDropdown();
-        }
-
-        /// <summary>
-        /// Logs unhandled exceptions to a file and prompts the user with the exception message.
-        /// </summary>
-        public static void OnUnhandledException(object sender, UnhandledExceptionEventArgs args)
-        {
-            // The unhandled exception
-            var unhandledException = args.ExceptionObject as Exception;
-
-            // MessageBox message
-            var message = new StringBuilder();
-            message.AppendLine("An unhandled error has occured:");
-            message.AppendLine();
-            message.AppendLine(unhandledException.GetFirstLine());
-            message.AppendLine();
-
-            // Create log if it hasn't been created yet
-            Logging.CreateMainLog();
-            // Use an alternate message if log couldn't be created
-            if (Logging.MainLogExists)
-            {
-                // Log exception
-                Logging.Main_WriteLine("-------------------");
-                Logging.Main_WriteLine("UNHANDLED EXCEPTION");
-                Logging.Main_WriteLine("-------------------");
-                Logging.Main_WriteException(unhandledException, "Unhandled exception!");
-
-                // Complete the message buffer
-                message.AppendLine("A log of the error has been created, do you want to open it?");
-
-                // Display message
-                var result = MessageBox.Show(message.ToString(), "Unhandled Error", MessageBoxButton.YesNo, MessageBoxImage.Error);
-                // If user requested to, open the log
-                if (result == MessageBoxResult.Yes)
-                {
-                    Process.Start(Logging.LogFolderPath);
-                }
-            }
-            else
-            {
-                // Complete the message buffer
-                message.AppendLine("An error log was unable to be created.");
-
-                // Display message
-                MessageBox.Show(message.ToString(), "Unhandled Error", MessageBoxButton.OK, MessageBoxImage.Error);
-            }
-
-            // Close the log files
-            Logging.CloseAll();
-            // Save settings
-            Settings.Default.Save();
-
-            // Close program
-            MessageBox.Show("The program will now shut down.", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
-            Application.Current.Shutdown();
         }
     }
 }
